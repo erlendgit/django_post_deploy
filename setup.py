24a4aa55eb446a17022dce61f4451374f7259a02
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-05-18 22:11:21 +0200

    Add README.md and LICENSE.txt to the project, update version on one location.

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 from setuptools import find_packages
+from post_deploy import VERSION
 
 setup(
     name='django_post_deploy',
     packages=find_packages(),
-    version='0.9.4',
+    version=VERSION[1:],
     license='cc-by-4.0',
     description='Add a generic method to have post-deploy actions done.',
     author='Erlend ter Maat',
     author_email='erwitema@gmail.com',
     url='https://github.com/erlendgit/django_post_deploy',
-    download_url='https://github.com/erlendgit/django_post_deploy/archive/refs/tags/v0.9.4.tar.gz',
+    download_url=f'https://github.com/erlendgit/django_post_deploy/archive/refs/tags/{VERSION}.tar.gz',
     keywords=['Django', 'Deployment', 'Management', 'CLI'],
     install_requires=[
         'Django',
