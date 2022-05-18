6deeb58a8bb7e84567123c00eb9b94ae68bb2786
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-05-18 21:42:59 +0200

    Add version to main package

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='django_post_deploy',
     packages=['post_deploy'],
-    version='0.9.1',
+    version='0.9.2',
     license='cc-by-4.0',
     description='Add a generic method to have post-deploy actions done.',
     author='Erlend ter Maat',
     author_email='erwitema@gmail.com',
     url='https://github.com/erlendgit/django_post_deploy',
-    download_url='https://github.com/erlendgit/django_post_deploy/archive/refs/tags/v0.9.1.tar.gz',
+    download_url='https://github.com/erlendgit/django_post_deploy/archive/refs/tags/v0.9.2.tar.gz',
     keywords=['Django', 'Deployment', 'Management', 'CLI'],
     install_requires=[
         'Django',
