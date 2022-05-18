908b796b1870018c0bf306b760eeafe78f0cf696
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-05-18 16:14:00 +0200

    Updated .gitignore with ignoreable files Updates setup.py after try-error uploading to PyPI

--- setup.py
+++ setup.py
@@ -8,17 +8,16 @@ setup(
     description='Add a generic method to have post-deploy actions done.',
     author='Erlend ter Maat',
     author_email='erwitema@gmail.com',
-    url='https://github.com/erlendgit/django_post_deploy',  # Provide either the link to your github or to your website
-    download_url='https://github.com/erlendgit/django_post_deploy/archive/v0.9.0.tar.gz',  # I explain this later on
-    keywords=['Django', 'Deployment', 'Management', 'CLI'],  # Keywords that define your package best
+    url='https://github.com/erlendgit/django_post_deploy',
+    download_url='https://github.com/erlendgit/django_post_deploy/archive/refs/tags/v0.9.0.tar.gz',
+    keywords=['Django', 'Deployment', 'Management', 'CLI'],
     install_requires=[
         'Django',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers :: Administrators :: Product owners',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: Creative Commons Attribution-ShareAlike 4.0',
         'Programming Language :: Python :: 3',
     ],
 )
