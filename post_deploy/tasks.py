f5c722b6121b7683a048d271243b35aa2afe997d
Erlend ter Maat <erlend@erlendtermaat.nl>
2025-12-10 17:17:25 +0100

    Recover from ModuleNotFoundError

--- post_deploy/tasks.py
+++ post_deploy/tasks.py
@@ -4,8 +4,11 @@ from post_deploy.plugins.scheduler.celery import CeleryScheduler
 app = CeleryScheduler.get_celery_app()
 
 
-@app.task
-def deploy_task(log_record_pks, context_parameters):
-    manager = get_context_manager(context_parameters)
-    with manager.execute():
-        run_deploy_action(log_record_pks)
+@app.task(bind=True)
+def deploy_task(self, log_record_pks, context_parameters):
+    try:
+        manager = get_context_manager(context_parameters)
+        with manager.execute():
+            run_deploy_action(log_record_pks)
+    except ModuleNotFoundError:
+        self.retry(countdown=300, max_retries=6)
